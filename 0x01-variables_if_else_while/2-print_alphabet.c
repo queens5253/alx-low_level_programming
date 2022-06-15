a9707c6d20af8f0ce50e6dce33860d2c46a93905
Sakina Hussein <queensakina52@gmail.com>
2022-06-15 11:19:39 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: always 0
+*/
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++);
+{
+putchar(ch);
+}
+putchar(10);
+
+return (0);
+}
