c0bab887b7fe3b34b5213beb346714659877cc56
Sakina Hussein <queensakina52@gmail.com>
2022-06-16 12:23:03 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit
+ * numbers of base 10 starting from 0
+ * Return: 0 always
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf(" ");
+	return (0);
+}
