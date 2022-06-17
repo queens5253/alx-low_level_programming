94974a752e82a2bdeeefcf5bf09c04e79815c497
Sakina Hussein <queensakina52@gmail.com>
2022-06-17 10:35:40 +0000

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - program that prints all possible
- * different combinations of two digits\
+ * different combinations of two digits
  * Return: 0 always
 */
 
@@ -9,14 +9,14 @@ int main(void)
 {
 int num1, num2;
 
-for (num1 = 0; num1 < 10; num1++)
+for (num1 = 0; num1 < 9; num1++)
 {
-for (num2 = 0; num2 < 10; num2++)
+for (num2 =  num1 + 1; num2 < 10; num2++)
 {
 putchar((num1 % 10) + '0');
 putchar((num2 % 10) + '0');
 
-if (num1 == 9 && num2 == 9)
+if (num1 == 8 && num2 == 9)
 continue;
 
 putchar(',');
