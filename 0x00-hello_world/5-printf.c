572a38a47e4ce033de090c9870c6800acb516273
Sakina Hussein <queensakina52@gmail.com>
2023-02-21 10:34:31 +0000

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,4 +2,11 @@
 
 /**
  * main - prints with proper grammar, but the outcome is a piece of art,
- *
+ * Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
