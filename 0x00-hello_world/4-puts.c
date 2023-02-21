d7659982704a188db7f5ac91718e498ddf1f72dc
Sakina Hussein <queensakina52@gmail.com>
2023-02-21 10:23:33 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly "Programming is like building a multilingual puzzle.
- * Return: 0
+  * main - print "Programming is like building a multilingual puzzle.
+  * Return: 0
 */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
